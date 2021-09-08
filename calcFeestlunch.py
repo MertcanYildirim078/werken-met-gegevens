fb2eea45abd801002c605474569631936c3b7474
MertcanYildirim078 <99068314@mydavinci.nl>
2021-09-08 10:33:20 +0200

    lol

---
+++ calcFeestlunch.py
@@ -0,0 +1,5 @@
+croissant = 0.39
+stokbrood = 2.78
+kortingsbon = 0.50
+
+print(croissant * 17 + (stokbrood * 2) - kortingsbon * 3)
---
+++ speelhaldag.py
@@ -0,0 +1,4 @@
+toegangsticket = 7.45
+vijfmin = 0.37 
+
+print((vijfmin * 9) * 3 + (3 * toegangsticket)   ) 
